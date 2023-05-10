e8c158b1e762df32c2d876e1d377dc7eccc177c7
chldyddnjs <dyddnjsdl321@naver.com>
2023-05-10 10:06:54 +0900

    second commit

---
+++ command.sh
@@ -0,0 +1,5 @@
+python img_cut.py --path '/home/yong/choi/hrviton/HR-VITON/data/test/image/00006_00.jpg' \
+                  --n 3
+python merge.py --path '/home/yong/choi/Deassignment/sub_images' \
+                --aug True\
+                --resize 256
